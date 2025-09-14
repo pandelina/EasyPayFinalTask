3fb193e40a5af89b06da11188e734b2933f73e61
pandelina <125447969+pandelina@users.noreply.github.com>
2025-09-14 21:49:19 +0200

    Update Contact.cs

--- updateddetyraep/Models/Contact.cs
+++ updateddetyraep/Models/Contact.cs
@@ -2,13 +2,12 @@
 {
     public class Contact
     {
-        public int Id { get; set; }                 // Primary key
-        public string? FirstName { get; set; }     // Emri i parë
-        public string? LastName { get; set; }      // Mbiemri
-        public string? Email { get; set; }         // Emaili
-        public string? Adress { get; set; }        // Adresa
+        public int Id { get; set; }                
+        public string? FirstName { get; set; }    
+        public string? LastName { get; set; }     
+        public string? Email { get; set; }        
+        public string? Adress { get; set; }        
 
-        // Lista e numrave të telefonit, lidhje me tabelën PhoneNumber
         public List<PhoneNumber> PhoneNumbers { get; set; } = new();
     }
 }
